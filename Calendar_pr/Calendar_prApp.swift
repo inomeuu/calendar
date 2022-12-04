3baf018fe1792d46d4e3cc5dc3be55a3b71ff1b8
Mei Inoue <inomei.0314@gmail.com>
2022-12-04 12:35:11 +0900

    Initial Commit

---
+++ Calendar_pr/Calendar_prApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calendar_prApp.swift
+//  Calendar_pr
+//
+//  Created by inoue mei on 2022/12/04.
+//
+
+import SwiftUI
+
+@main
+struct Calendar_prApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calendar_pr/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Calendar_pr
+//
+//  Created by inoue mei on 2022/12/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
